556ea45e04a59599924c0020fb57f2def39608f3
Mangeshi <thisismangeshi@gmail.com>
2021-03-13 19:54:09 +0530

    requested an api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const requestApi = async () => {
+        const response = await fetch("https://type.fit/api/quotes");
+        const data = await response.json();
+        console.log(data);
+    };
+    requestApi();
+    return (
+        <button type="button" onClick={handleChangeQuote}>
+            next quote
+        </button>
+    );
 }
 
 export default App;
