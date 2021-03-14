e0a819fd4c1e5f9f11827939443c78c7587c3c2c
Mangeshi <thisismangeshi@gmail.com>
2021-03-14 12:52:02 +0530

    added styling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-    const [quote, setQuote] = useState("");
-
+    const [quote, setQuote] = useState(" ");
 
     const requestApi = () => {
         fetch("https://type.fit/api/quotes")
-        .then((res)=>res.json())
-        .then((data)=>{
-        let randomNum=Math.floor(Math.random()*data.length);
-        setQuote(data[randomNum])
-        }
-        )
-
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                let randomNum = Math.floor(Math.random() * data.length);
+                setQuote(data[randomNum]);
+            });
     };
 
-
-
+    useEffect(() => {
+        requestApi();
+    }, []);
 
     return (
         <>
-            <button type="button" onClick={requestApi}>next quote</button>
-            <h1>{quote.text}</h1>
+            <div className="container">
+                <div className="subcontainer">
+                    <div className="quote">
+                        <h5>{`“${quote.text}"`}</h5>
+                    </div>
+                    <div className="author">
+                        <h4>{`~${quote.author}`}</h4>
+                    </div>
+                    <div className="button">
+                        <button type="button" onClick={requestApi}>
+                            New Quote
+                        </button>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
