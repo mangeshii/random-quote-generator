b311c90740f8b27d537a0c34359e4b9abde6cf47
Mangeshi <thisismangeshi@gmail.com>
2021-11-20 01:54:51 +0530

    added random quote generator

--- src/App.js
+++ src/App.js
@@ -1,76 +1,56 @@
 import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import opening from "./Images/opening.png";
+import closing from "./Images/closing.png";
 
 function App() {
-    const [quote, setQuote] = useState(" ");
-    const [changeBackground, setChangeBackground] = useState("green");
-    const colors = [
-        "#FF007F",
-        "E0115F",
-        "#4C5270",
-        "#7A4988",
-        "#601A35",
-        "#4D0F28",
-        "#311432",
-        "#228C22",
-        "#ff781f",
-        "#116DB6",
-        "#00239CFF",
-        "#FFB03E",
-        "#165BAA",
-        "0B1354",
-        "0b032d",
-        "#7584AD",
-        "#000000",
-    ];
+    const [quote, setQuote] = useState("");
+    console.log(quote);
 
-    const requestApi = () => {
-        fetch("https://type.fit/api/quotes")
-            .then((res) => res.json())
-            .then((data) => {
-                let randomNum = Math.floor(Math.random() * data.length);
-                setQuote(data[randomNum]);
-                let randomQuote = Math.floor(Math.random() * 17);
-                setChangeBackground(colors[randomQuote]);
-            });
+    const GetQuote = async () => {
+        const quoteResponse = await fetch("https://type.fit/api/quotes");
+        const quoteData = await quoteResponse.json();
+
+        const GenerateRandomNumber = () => {
+            let randomNum = Math.floor(Math.random() * quoteData.length);
+            setQuote(quoteData[randomNum]);
+        };
+        GenerateRandomNumber();
     };
 
     useEffect(() => {
-        requestApi();
+        GetQuote();
     }, []);
 
     return (
-        <>
-            <div
-                className="container"
-                style={{ backgroundColor: changeBackground }}
-            >
-                <div className="subcontainer">
-                    <div className="quote" style={{ color: changeBackground }}>
-                        <i class="fa fa-quote-left">{`  ${quote.text}"`}</i>
-                    </div>
-                    <div className="author" style={{ color: changeBackground }}>
-                        <h4>{`~${quote.author}`}</h4>
-                    </div>
-                    <div className="button">
-                        <button style={{ backgroundColor: changeBackground }}>
-                            <a
-                                href={`https://twitter.com/intent/tweet?text=${quote.text}`}
-                            >
-                                Tweet
-                            </a>
-                        </button>
-                        <button
-                            type="button"
-                            onClick={requestApi}
-                            style={{ backgroundColor: changeBackground }}
-                        >
-                            New Quote
-                        </button>
-                    </div>
+        <div className="wrapper container-fluid">
+            <div className=" containers">
+                <div className="tweet-btn">
+                    <a
+                        href={`https://twitter.com/intent/tweet?text=${quote.text}`}
+                    >
+                        <button className="tweet">Tweet</button>
+                    </a>
+                </div>
+                <div className="opening-quote">
+                    <img src={opening} alt="openingtag" />
+                </div>
+                <div className="quote-container">
+                    <div className="quote-content">{quote.text}</div>
+                </div>
+                <div className="closing-quote">
+                    <img src={closing} alt="closingtag" />
+                </div>
+                <div className="author">{`~${quote.author}`}</div>
+                <div className="quote-btn">
+                    <button className="new-quote" onClick={GetQuote}>
+                        New quote
+                    </button>
                 </div>
             </div>
-        </>
+        </div>
     );
+
 }
 
 export default App;
